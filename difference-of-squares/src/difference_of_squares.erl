abccd54fddd6949c381d1ef19a2e5820cd512b8d
Richard Wild <richardwild76@gmail.com>
2018-11-22 10:59:03 +0000

    Sum of squares iteration 2

--- difference-of-squares/src/difference_of_squares.erl
+++ difference-of-squares/src/difference_of_squares.erl
@@ -3,10 +3,10 @@
 -export([sum_of_squares/1, square_of_sum/1, difference_of_squares/1, test_version/0]).
 
 sum_of_squares(0) -> 0;
-sum_of_squares(N) -> (N * N) + sum_of_squares(N - 1).
+sum_of_squares(N) -> square(N) + sum_of_squares(N - 1).
+square(N) -> N * N.
 
-square_of_sum(N) -> Sum = sum(N), Sum * Sum.
-
+square_of_sum(N) -> square(sum(N)).
 sum(0) -> 0;
 sum(N) -> N + sum(N - 1).
 
