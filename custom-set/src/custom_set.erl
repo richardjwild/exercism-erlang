45f2daac80f394044ea4a63f5dacfc4d3bc4eb1c
Richard Wild <richardwild76@gmail.com>
2019-02-01 21:21:06 +0000

    Custom set all test pass

--- custom-set/src/custom_set.erl
+++ custom-set/src/custom_set.erl
@@ -1,27 +1,68 @@
 -module(custom_set).
 
 -export([add/2, contains/2, difference/2, disjoint/2, empty/1, equal/2, from_list/1, intersection/2, subset/2,
-	 union/2, test_version/0]).
+  union/2, test_version/0]).
 
 
-add(_Elem, _Set) -> undefined.
+add(_Elem, _Set) ->
+  case (contains(_Elem, _Set)) of
+    false -> lists:sort(_Set ++ [_Elem]);
+    true -> _Set
+  end.
 
-contains(_Elem, _Set) -> undefined.
+contains(_Elem, _Set) -> lists:search(fun(T) -> T == _Elem end, _Set) == {value, _Elem}.
 
-difference(_Set1, _Set2) -> undefined.
+difference([], _) -> [];
+difference(_Set1, []) -> lists:sort(_Set1);
+difference(_Set1, _Set2) ->
+  [_Elem | _Rest] = _Set2,
+  case (contains(_Elem, _Set1)) of
+    true -> difference(lists:delete(_Elem, _Set1), _Rest);
+    false -> difference(_Set1, _Rest)
+  end.
 
-disjoint(_Set1, _Set2) -> undefined.
+disjoint([], _) -> true;
+disjoint(_, []) -> true;
+disjoint(_Set1, _Set2) ->
+  [_Elem | _Rest] = _Set1,
+  case (contains(_Elem, _Set2)) of
+    false -> disjoint(_Rest, _Set2);
+    true -> false
+  end.
 
-empty(_Set) -> undefined.
+empty(_Set) -> length(_Set) == 0.
 
-equal(_Set1, _Set2) -> undefined.
+equal([], []) -> true;
+equal(_Set1, _Set2) -> subset(_Set1, _Set2) and subset(_Set2, _Set1).
 
-from_list(_List) -> undefined.
+from_list([], _Result) -> _Result;
+from_list(_List, _Result) ->
+  [_Elem | _Rest] = _List,
+  from_list(_Rest, add(_Elem, _Result)).
 
-intersection(_Set1, _Set2) -> undefined.
+from_list(_List) -> from_list(_List, []).
 
-subset(_Set1, _Set2) -> undefined.
+intersection([], _, _Result) -> _Result;
+intersection(_Set1, _Set2, _Result) ->
+  [_Elem | _Rest] = _Set1,
+  case (contains(_Elem, _Set2)) of
+    true -> intersection(_Rest, _Set2, _Result ++ [_Elem]);
+    false -> intersection(_Rest, _Set2, _Result)
+  end.
 
-union(_Set1, _Set2) -> undefined.
+intersection([], _) -> [];
+intersection(_, []) -> [];
+intersection(_Set1, _Set2) -> intersection(_Set1, _Set2, []).
+
+subset([], _) -> true;
+subset(_Set1, _Set2) ->
+  [_Elem | _Rest] = _Set1,
+  case (contains(_Elem, _Set2)) of
+    true -> subset(_Rest, _Set2);
+    false -> false
+  end.
+
+union([], []) -> [];
+union(_Set1, _Set2) -> from_list(lists:append(_Set1, _Set2)).
 
 test_version() -> 2.
