d399ee68c68a310a1e99e7710ea74cef494871c6
Richard Wild <richardwild76@gmail.com>
2019-02-02 20:45:54 +0000

    Use Erlang pattern matching to simplify

--- queen-attack/src/queen_attack.erl
+++ queen-attack/src/queen_attack.erl
@@ -2,13 +2,7 @@
 
 -export([can_attack/2]).
 
-can_attack(_WhiteX, _WhiteY, _BlackX, _BlackY)
-  when (_WhiteX == _BlackX) -> true;
-can_attack(_WhiteX, _WhiteY, _BlackX, _BlackY)
-  when (_WhiteY == _BlackY) -> true;
-can_attack(_WhiteX, _WhiteY, _BlackX, _BlackY)
-  when (abs(_WhiteX - _BlackX) == abs(_WhiteY - _BlackY)) -> true;
-can_attack(_, _, _, _) -> false.
-
-can_attack({WhiteX, WhiteY}, {BlackX, BlackY}) ->
-  can_attack(WhiteX, WhiteY, BlackX, BlackY).
+can_attack({X, _}, {X, _}) -> true;
+can_attack({_, Y}, {_, Y}) -> true;
+can_attack({Wx, Wy}, {Bx, By}) when (abs(Wx - Bx) == abs(Wy - By)) -> true;
+can_attack(_, _) -> false.
